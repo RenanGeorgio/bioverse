352994855633066885fb093577ced61fdd945cbb
RenanGeorgio <regeo9@hotmail.com>
2025-02-18 21:55:19 -0300

    fix

--- lib/supabase/queries.ts
+++ lib/supabase/queries.ts
@@ -4,7 +4,7 @@ import { Database } from '@/lib/schema';
 
 type Question = Database['public']['Tables']['todos']['Row']
 
-type QuestionsProps = {
+type QuestionsProps = (supabase: SupabaseClient) => {
     questions: Question[];
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     error?: any;
@@ -19,14 +19,14 @@ export const getUser = cache(async (supabase: SupabaseClient) => {
   return user;
 });
 
-export const getQuestions: QuestionsProps = async (supabase: SupabaseClient) => {
+export const getQuestions: QuestionsProps = cache(async (supabase: SupabaseClient) => {
   const { data: questions, error } = await supabase
     .from('todos')
     .select('*')
     .order('id', { ascending: true });
 
   return { questions, error };
-};
+});
 
 export const addQuestions = async (supabase: SupabaseClient, task: string, id: string | number) => {
     const { data: todo, error } = await supabase
