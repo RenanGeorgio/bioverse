99d598f07ddd35371e88991fe78a3322170364cc
RenanGeorgio <regeo9@hotmail.com>
2025-02-21 22:04:02 -0300

    feat
    1) account
    2) redirect
    3) auth

---
+++ app/signin/[id]/page.tsx
@@ -0,0 +1,51 @@
+import Logo from '@/components/icons/Logo';
+import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { getViewTypes, getDefaultSignInView } from '@/utils/auth-helpers/settings';
+
+
+export default async function SignIn({
+  params,
+  searchParams
+}: {
+  params: { id: string };
+  searchParams: { disable_button: boolean };
+}) {
+  const viewTypes = getViewTypes();
+
+  // Declare 'viewProp' and initialize with the default value
+  let viewProp: string;
+
+  // Assign url id to 'viewProp' if it's a valid string and ViewTypes includes it
+  if (typeof params.id === 'string' && viewTypes.includes(params.id)) {
+    viewProp = params.id;
+  } else {
+    const preferredSignInView = cookies().get('preferredSignInView')?.value || null;
+    viewProp = getDefaultSignInView(preferredSignInView);
+    return redirect(`/signin/${viewProp}`);
+  }
+
+  // Check if the user is already logged in and redirect to the account page if so
+  const supabase = createClient();
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  if (user && viewProp !== 'update_password') {
+    return redirect('/');
+  } else if (!user && viewProp === 'update_password') {
+    return redirect('/signin');
+  }
+
+  return (
+    <div className="flex justify-center height-screen-helper">
+      <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
+        <div className="flex justify-center pb-12 ">
+          <Logo width="64px" height="64px" />
+        </div>
+      </div>
+    </div>
+  );
+}
